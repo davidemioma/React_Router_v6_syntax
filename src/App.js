59506172504dca299a2bc7cb32afac089fbf5f24
davidemioma <davidemiomauche@gmail.com>
2022-01-07 17:29:47 +0100

    page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes, Navigate } from "react-router-dom";
 
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-import MainHeader from './components/MainHeader';
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-        <Switch>
-          <Route path='/welcome'>
-            <Welcome />
+        <Routes>
+          <Route path="/" element={<Navigate to="/welcome" />} />
+
+          <Route path="/welcome/*" element={<Welcome />}>
+            <Route path="new-user" element={<p>Welcome, new user!</p>} />
           </Route>
-          <Route path='/products' exact>
-            <Products />
-          </Route>
-          <Route path='/products/:productId'>
-            <ProductDetail />
-          </Route>
-        </Switch>
+
+          <Route path="/products" element={<Products />} />
+
+          <Route path="/products/:productId" element={<ProductDetail />} />
+        </Routes>
       </main>
     </div>
   );
 }
 
 export default App;
+
+//Instead of useHistory() we use use Navigation.
+//const navigate = useNavigatinon();
+//navigate('page', {push: true, or replace: true})
 
 // our-domain.com/welcome => Welcome Component
 // our-domain.com/products => Products Component
